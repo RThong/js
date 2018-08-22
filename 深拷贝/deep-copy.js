8e821bfcd8193edbd2f8529b5ce4f82f12927c40
rthong <403216075@qq.com>
2018-08-22 21:36:32 +0800

    22

--- 深拷贝/deep-copy.js
+++ 深拷贝/deep-copy.js
@@ -1,10 +1,16 @@
-function clone(obj){    
-	if(!obj&& typeof obj!== 'object'){      
-		return;    
+//判断深拷贝不需要考虑对象是函数函数
+function isRef(target) {
+	return (Object.prototype.toString.call(target) === '[object Object]' || Object.prototype.toString.call(target) === '[object Array]')
+}
+
+function clone(obj) {
+	if(!isRef(obj)) {
+		return new Error('这不是一个对象或者数组')
 	}
-	var newObj=obj.constructor===Object?{}:[];    
-	for(var key in obj){              
-		newObj[key] =(obj[key]&&typeof obj[key]==='object')?clone(obj[key]):obj[key];       
-	}    
-	return newObj; 
+	let result = obj.constructor === Object ? {} : []
+	for(let key in obj) {
+		//函数直接复制
+		result[key] = isRef(obj[key]) ? clone(obj[key]) : obj[key]
+	}
+	return result
 }
