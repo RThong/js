f49ebe0186b5f417d8b7284387a353c7f2c24170
brant <403216075@qq.com>
2021-04-20 16:39:15 +0800

    [fix]修改接收的参数

--- 模拟new/index.js
+++ 模拟new/index.js
@@ -4,24 +4,24 @@
 //如果构造函数没有显示返回对象，就返回该临时对象
 
 function A(options) {
-  this.name = options.name
+  this.name = options.name;
 }
-A.prototype.fn = function() {
+A.prototype.fn = function () {
   console.log(this.name);
-}
+};
 
 function fakeNew() {
-  const targetFn = arguments[0]
-  const options = arguments[1] || undefined
-  let instance = {}
+  const targetFn = arguments[0];
+  const options = [].slice.call(arguments, 1);
+  let instance = {};
   // let instance = Object.create(targetFn.prototype)
 
-  let res = targetFn.call(instance, options)
-  console.log(Object.setPrototypeOf(instance, targetFn.prototype))
-  return typeof res === 'Object' ? res : instance
+  const res = targetFn.apply(instance, options);
+  console.log(Object.setPrototypeOf(instance, targetFn.prototype));
+  return typeof res === "Object" ? res : instance;
 }
 
-var instance = fakeNew(A, {name: 'hong'})
+var instance = fakeNew(A, { name: "hong" });
 console.dir(instance);
-instance.fn()
-console.dir(new A({name: 'hong'}));
+instance.fn();
+console.dir(new A({ name: "hong" }));
