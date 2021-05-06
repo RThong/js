658067ca6b4eaaf9014d4ed5e05942a09240dadd
brant <403216075@qq.com>
2021-05-06 11:15:50 +0800

    [feat]重新实现深拷贝

--- 深拷贝/deep-copy.js
+++ 深拷贝/deep-copy.js
@@ -1,16 +1,41 @@
-//判断深拷贝不需要考虑对象是函数函数
-function isRef(target) {
-	return (Object.prototype.toString.call(target) === '[object Object]' || Object.prototype.toString.call(target) === '[object Array]')
+// https://juejin.cn/post/6844903929705136141
+function deepCopy(target, map = new Map()) {
+  if (typeof target === "object" && target !== null) {
+    let cloneTarget = target instanceof Array ? [] : {};
+
+    // 处理循环应用的情况   通过map来按照key为原数据值为拷贝数据的形式来看是否拷贝过这个对象
+    if (map.get(target)) {
+      return map.get(target);
+    }
+
+    map.set(target, cloneTarget);
+
+    for (const key in target) {
+      cloneTarget[key] = deepCopy(target[key], map);
+    }
+    return aaa;
+  }
+
+  return target;
 }
 
-function clone(obj) {
-	if(!isRef(obj)) {
-		return new Error('这不是一个对象或者数组')
-	}
-	let result = obj.constructor === Object ? {} : []
-	for(let key in obj) {
-		//函数直接复制
-		result[key] = isRef(obj[key]) ? clone(obj[key]) : obj[key]
-	}
-	return result
+// 拷贝函数
+let cloneFn = (fn) => {
+  return new Function("return " + fn.toString())();
+};
+
+function fn(aaa) {
+  console.log(aaa);
 }
+
+// 测试
+const target = {
+  field1: 1,
+  field2: undefined,
+  field3: {
+    child: "child",
+  },
+  field4: [2, 4, 8],
+  field5: null,
+};
+target.target = target;
